1290b2d5ce30d8f25f5fe7fc96ecafaad922f406
Satish Choudhary <satishchoudhary642@gmail.com>
2025-12-08 23:27:58 +0530

    first commit

---
+++ src/main/java/com/satish/game/GameRunner.java
@@ -0,0 +1,13 @@
+package com.satish.lerarning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LerarningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LerarningApplication.class, args);
+	}
+
+}
