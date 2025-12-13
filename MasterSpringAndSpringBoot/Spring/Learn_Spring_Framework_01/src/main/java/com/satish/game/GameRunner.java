597acbffcc921c86247366e4cc6ea6445fa934be
Satish Choudhary <satishchoudhary642@gmail.com>
2025-12-13 11:53:20 +0530

    Fix Learn_Spring_Framework_01 embedded repo

---
+++ MasterSpringAndSpringBoot/Spring/Learn_Spring_Framework_01/src/main/java/com/satish/game/GameRunner.java
@@ -0,0 +1,18 @@
+package com.satish.game;
+
+public class GameRunner {
+
+    private GamingConsole game;
+
+    GameRunner(GamingConsole game){
+        this.game=game;
+    }
+
+    public void run(){
+        System.out.println("Running game: " + game);
+        game.up();
+        game.down();
+        game.left();
+        game.right();
+    }
+}
---
+++ MasterSpringAndSpringBoot/Spring/Learn_Spring_Framework_01/src/test/java/com/satish/lerarning/LerarningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satish.lerarning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LerarningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
