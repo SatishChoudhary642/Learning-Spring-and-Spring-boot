9ae368ab1aa47cbe9ec30ac47bf4d1255737b7a8
Satish Choudhary <satishchoudhary642@gmail.com>
2025-12-13 12:28:26 +0530

    Add Learn_Spring_Framework_02 project

---
+++ Spring/Learn_Spring_Framework_02/src/main/java/com/satish/game/GamingConsole.java
@@ -0,0 +1,8 @@
+package com.satish.game;
+
+public interface GamingConsole {
+    void up();
+    void down();
+    void left();
+    void right();
+}
---
+++ Spring/Learn_Spring_Framework_02/src/main/java/com/satish/game/MarioGame.java
@@ -0,0 +1,16 @@
+package com.satish.game;
+
+public class MarioGame implements GamingConsole{
+    public void up(){
+        System.out.println("Jump");
+    }
+    public void down(){
+        System.out.println("Go in the hole");
+    }
+    public void left(){
+        System.out.println("go back");
+    }
+    public void right(){
+        System.out.println("Move forward");
+    }
+}
