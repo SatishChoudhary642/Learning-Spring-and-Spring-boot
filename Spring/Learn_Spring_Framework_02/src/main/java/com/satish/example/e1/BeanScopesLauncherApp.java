ea2cc62837383adef2917771fdd251de029a8059
Satish <satishchoudhary642@gmail.com>
2025-12-17 00:37:22 +0530

    Scope types

--- Spring/Learn_Spring_Framework_02/src/main/java/com/satish/example/e1/BeanScopesLauncherApp.java
+++ Spring/Learn_Spring_Framework_02/src/main/java/com/satish/example/e1/BeanScopesLauncherApp.java
@@ -27,13 +27,21 @@ public class BeanScopesLauncherApp {
 		
 		try(var context =new  AnnotationConfigApplicationContext(BeanScopesLauncherApp.class)){
 
+			//Same bean(Instance) for all calls --> Singleton
 			System.out.println(context.getBean(NormalClass.class));
 			System.out.println(context.getBean(NormalClass.class));
 
+			// different instance for each time bean is refered to
+			//-->Prototype
 			System.out.println(context.getBean(PrototypeClass.class));
 			System.out.println(context.getBean(PrototypeClass.class));
 			System.out.println(context.getBean(PrototypeClass.class));
-		}
+
+			//There are more Spring bean Scopes like -
+			//Request --> one object instance per single HTTP request
+			//Session --> one object instance per user HTTP Session
+			//Application --> one object instance per web application rutime
+			//websocket --> one object instance per websocket instance
 	}
 
 }
