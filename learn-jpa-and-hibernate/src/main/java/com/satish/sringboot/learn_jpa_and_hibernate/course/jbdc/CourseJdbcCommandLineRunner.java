3e1cc9820c86d910c6ce74aed3424e1fe6388a36
Satish <satishchoudhary642@gmail.com>
2025-12-26 02:27:32 +0530

    using JDBC

--- learn-jpa-and-hibernate/src/main/java/com/satish/sringboot/learn_jpa_and_hibernate/course/jbdc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/satish/sringboot/learn_jpa_and_hibernate/course/jbdc/CourseJdbcCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.satish.sringboot.learn_jpa_and_hibernate.course.Course;
+
 
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner{
@@ -13,7 +15,18 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+
+        Course c1 = new Course(1, "Learn C++ DSA", "LoveBabar");
+        Course c2 = new Course(2, "Learn spring boot", "in28minutes");
+        Course c3 = new Course(3, "Web dev", "Chai with code");
+        Course c4 = new Course(4, "devOPs", "in28minutes");
+        
+        repository.insert(c1);
+        repository.insert(c2);
+        repository.insert(c3);
+        repository.insert(c4);
+
+        repository.deleteById(2);
     }
     
 }
