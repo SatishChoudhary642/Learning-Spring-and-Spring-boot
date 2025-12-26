f4ae4cfe83be1ddd0a46080ebcb96aa01ff3061c
Satish <satishchoudhary642@gmail.com>
2025-12-27 01:00:41 +0530

    Refactor project structure and add actuator dependency; update CourseController to return a list of courses and configure management endpoints

--- learning-spring-boot/src/main/java/com/satish/springboot/learning_spring_boot/CourseControler.java
+++ learning-spring-boot/src/main/java/com/satish/springboot/learning_spring_boot/CourseControler.java
@@ -1,5 +1,8 @@
 package com.satish.springboot.learning_spring_boot;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CourseControler {
 
-    @Autowired
-    private CurrencyServiceConfig config;
-
     @RequestMapping("/course")
-    public CurrencyServiceConfig getAllCourses(){
-        return config;
+    public List<Course> getAllCourses(){
+        return Arrays.asList(
+            new Course(1, "Mern", "Code with chai"),
+            new Course(2, "DSA in C++", "LoveBabar"),
+            new Course(3,"Spring boot", "in28minutes")
+        );
     }
 }
