9b161bda59229f7b0a772636452620b54834674e
Satish <satishchoudhary642@gmail.com>
2025-12-28 00:43:01 +0530

    JSP

--- myfirstwebapp/src/main/java/com/satish/springboot/myfirstwebapp/hello/SayHelloController.java
+++ myfirstwebapp/src/main/java/com/satish/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -29,4 +29,15 @@ public class SayHelloController {
 
         return sb.toString();
     }
+
+   
+    //src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+    
+    //http:..../say-hello-jsp => SayHelloController => method => sayHello
+    //=> prefix +  sayHello + suffix
+    //=> src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+    @RequestMapping("say-hello-jsp")
+    public String sayHelloJsp(){
+        return "sayHello";
+    }
 }
---
+++ myfirstwebapp/src/main/java/com/satish/springboot/myfirstwebapp/login/LoginController.java
@@ -0,0 +1,13 @@
+package com.satish.springboot.myfirstwebapp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    
+    @RequestMapping("login")
+    public String gottoLoginPage(){
+        return "login";
+    }
+}
