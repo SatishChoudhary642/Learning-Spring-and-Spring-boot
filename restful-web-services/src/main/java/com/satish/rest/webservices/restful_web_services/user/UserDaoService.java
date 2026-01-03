17d804bf72da114b370d23121d42c53fd35e2fe6
Satish <satishchoudhary642@gmail.com>
2026-01-04 01:52:51 +0530

    implement User REST API (GET/POST) and global exception handling

---
+++ restful-web-services/src/main/java/com/satish/rest/webservices/restful_web_services/user/UserDaoService.java
@@ -0,0 +1,39 @@
+package com.satish.rest.webservices.restful_web_services.user;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class UserDaoService {
+    private static List<User> users= new ArrayList<>();
+
+    private static int  usersCount=0;
+
+    static {
+        users.add(new User(++usersCount,"Satish", LocalDate.now().minusYears(21)));
+        users.add(new User(++usersCount,"Rahul", LocalDate.now().minusYears(17)));
+        users.add(new User(++usersCount,"Krishna", LocalDate.now().minusYears(9)));
+    }
+
+    public List<User> findAll(){
+        return users;
+    }
+
+    public  User findOne(int id){
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream()
+                .filter(predicate)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public User save(User user){
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
+}
---
+++ restful-web-services/src/main/java/com/satish/rest/webservices/restful_web_services/user/UserResource.java
@@ -0,0 +1,44 @@
+package com.satish.rest.webservices.restful_web_services.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retriveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retriveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if(user==null){
+            throw new UserNotFoundException("id: "+id);
+        }
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                        .fromCurrentRequest().path("/{id}")
+                        .buildAndExpand(savedUser.getId())
+                        .toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
