3a3feb38550682ce26961bb2aa62d9f15e64f9ab
Satish <satishchoudhary642@gmail.com>
2026-01-12 00:07:10 +0530

    Add performance tracking aspect and implement BusinessService1 and DataService1 for data processing

---
+++ learn-spring-aop/src/main/java/com/satish/learn_spring_aop/aopexample/business/BusinessService2.java
@@ -0,0 +1,29 @@
+package com.satish.learn_spring_aop.aopexample.business;
+
+import com.satish.learn_spring_aop.aopexample.data.DataService1;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class BusinessService1 {
+
+    private DataService1 dataService1;
+
+    public BusinessService1(DataService1 dataService1) {
+        this.dataService1 = dataService1;
+    }
+
+    public int calculateMax(){
+        int[] data = dataService1.retrieveData();
+
+        // making method sleep to see time on findExecutionTime
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        // throw new RuntimeException("Something went wrong");
+        return Arrays.stream(data).max().orElse(0);
+    }
+}
