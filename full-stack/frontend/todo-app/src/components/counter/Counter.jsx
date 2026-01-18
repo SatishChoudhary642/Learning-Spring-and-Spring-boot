1cfb631d6dade7562ebb374bfb93151865694855
Satish <satishchoudhary642@gmail.com>
2026-01-19 00:44:45 +0530

    feat: refactor Counter component to use destructured state for clarity

--- full-stack/frontend/todo-app/src/components/counter/Counter.jsx
+++ full-stack/frontend/todo-app/src/components/counter/Counter.jsx
@@ -4,16 +4,15 @@ import './Counter.css'
 export default function Counter(){
 
     //[0,f]
-    const state = useState(0)
+    const [count, setCount] = useState(0)
 
     function incrementCounterFunction(){
-        var cnt=
-        state[1](state[0]+1);
+        setCount(count+1);
     }
 
     return (
         <div className="Counter">
-            <span className="count">{state[0]}</span>
+            <span className="count">{count}</span>
             <div>
                 <button className="counterButton" 
                     onClick={incrementCounterFunction}
